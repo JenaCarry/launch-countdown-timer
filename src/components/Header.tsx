a9a23ddf81794944f4ca068da5ef36615cbaa89d
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-07-30 15:15:58 -0300

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <header className="self-end">
-      <h1 className="w-full max-[500px]:max-w-xs text-xl sm:text-2xl uppercase text-white mx-auto tracking-[0.25rem]">
+      <h1 className="w-full max-[500px]:max-w-xs text-xl uppercase text-white mx-auto tracking-[0.25rem]">
         We're launching soon
       </h1>
     </header>
